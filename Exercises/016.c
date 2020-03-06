99cfe62f492f9dc1bc53b71973ee08247d20c601
Ayşıl Simge <aysilsimgekaracan@hotmail.com>
2020-03-06 17:53:52 +0300

    Find ASCII value
    Find ASCII value

---
+++ Exercises/016.c
@@ -0,0 +1,32 @@
+//
+//  main.c
+//  016
+//
+//  Created by Ayşıl Simge Karacan on 9.11.2019.
+//  Copyright © 2019 Ayşıl Simge Karacan. All rights reserved.
+//
+
+//
+//Kaynak:https://www.programiz.com/article/algorithm-programming
+//
+
+//Find ASCII value
+
+#include <stdio.h>
+
+int main(void) {
+    
+    //VARIABLES
+    char ascii;
+    
+    printf("Insert a charater: ");
+    scanf("%c",&ascii);
+    
+    printf("The ASCII value of the %c is: %d\n",ascii,ascii);
+    
+    
+    
+    return 0;
+}
+
+
