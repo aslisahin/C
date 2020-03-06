8b437456ea84584efcfd5a53119bf9e52eed62bf
Ayşıl Simge <aysilsimgekaracan@hotmail.com>
2020-03-06 17:54:23 +0300

    Write a program that prints the following shapes with asterisks.
    Write a program that prints the following shapes with asterisks.

---
+++ Exercises/021.c
@@ -0,0 +1,26 @@
+//
+//  main.c
+//  021
+//
+//  Created by Ayşıl Simge Karacan on 13.11.2019.
+//  Copyright © 2019 Ayşıl Simge Karacan. All rights reserved.
+//
+
+//Write a program that prints the following shapes with asterisks. (Introduction to C programming page 97)
+
+#include <stdio.h>
+
+int main(void) {
+    
+    printf("*********\t   ***   \t  *  \t\n");
+    printf("*       *\t *     * \t ***\n");
+    printf("*       *\t*       *\t*****\n");
+    printf("*       *\t*       *\t  *\n");
+    printf("*       *\t*       *\t  *\n");
+    printf("*       *\t*       *\t  *\n");
+    printf("*       *\t*       *\t  *\n");
+    printf("*       *\t *     * \t  *\n");
+    printf("*********\t   ***   \t  *  \t\n");
+    
+    return 0;
+}
